63ce683baf78c9a384e73bdf707dcb68730123d4
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-02-18 14:52:14 +0600

    add function to delete task

--- api-todo/routers/tasks.ts
+++ api-todo/routers/tasks.ts
@@ -86,4 +86,32 @@ tasksRouter.put('/:id', auth, async (req: RequestWithUser, res, next) => {
   }
 });
 
+tasksRouter.delete('/:id', auth, async (req: RequestWithUser, res, next) => {
+  try {
+    const taskId = req.params.id;
+
+    let _id: Types.ObjectId;
+    try {
+      _id = new Types.ObjectId(req.user?._id);
+    } catch {
+      return res.status(404).send({ error: 'Wrong ObjectId!' });
+    }
+
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      return res.status(404).send({ error: 'Task not found!' });
+    }
+
+    if (!task.user.equals(_id)) {
+      return res.status(403).send({ error: 'You cannot delete this task' });
+    }
+
+    await Task.deleteOne({ _id: taskId });
+    return res.status(200).send({ message: `Task delete id = ${taskId}` });
+  } catch (e) {
+    next(e);
+  }
+});
+
 export default tasksRouter;
